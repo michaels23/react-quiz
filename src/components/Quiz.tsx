929b073bf4e363a813880605169f17ed2d0f813d
Mike Steigerwald <Steiger@UMich.edu>
2021-05-15 14:50:43 -0500

    Move answer form into Multiple Choice Question

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,59 +1,27 @@
 import { FC } from "react";
-import { Alternative, AlternativeProps } from "./Alternative";
-import { Question } from "./Question";
 import { QuestionCounter } from "./QuestionCounter";
+import {alternative, MultipleChoiceQuestion, question} from "./MultipleChoiceQuestion";
 
-type QuizProps = {
-  answer: string;
-  answerOptions: AlternativeProps[];
+export type QuizProps = {
+  alternatives: alternative[];
   counter: number;
-  question: string;
-  questionId: number;
+  question: question;
   questionTotal: number;
   selectSize: number;
   onAnswerSelected: (value: any) => void;
 };
 
 export const Quiz: FC<QuizProps> = (props: QuizProps) => {
-  function renderAnswerOptions(key: AlternativeProps) {
-    return (
-      <Alternative
-        key={key.content}
-        content={key.content}
-        id={key.id}
-      />
-    );
-  }
-
   return (
     <div className="quiz">
       <QuestionCounter
-        currentNumber={props.questionId}
+        currentNumber={props.question.id}
         total={props.questionTotal}
       />
-      <Question content={props.question} />
-      <form id="answerForm">
-        <label htmlFor={`question_${props.questionId}`} className="sr-only">
-          Select an answer:
-        </label>
-        <select
-          size={props.selectSize}
-          className="answerOptions"
-          id={`question_${props.questionId}`}
-          name={`question_${props.questionId}`}
-          required={true}
-        >
-          {props.answerOptions.map(renderAnswerOptions)}
-        </select>
-        <button
-          type="button"
-          className="submit"
-          aria-label="Submit"
-          onClick={props.onAnswerSelected}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+      <MultipleChoiceQuestion 
+      alternatives={props.alternatives} 
+      question={props.question}
+      selectSize={props.selectSize}
+      onAnswerSelected={props.onAnswerSelected}/>    </div>
   );
 };
